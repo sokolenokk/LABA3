25501699f4d06c15003f1a11f21cdad8f2bcbf2a
sokolenok <skls77@mail.ru>
2025-11-16 02:40:18 +0300

    add price calculator context and output (task 4)

--- task4.py
+++ task4.py
@@ -32,3 +32,35 @@ class FixedDiscountStrategy(PricingStrategy):
     def calculate(self, base_price: float) -> float:
         answer = base_price - self._discount_amount
         return max(answer, 0.0)
+
+
+class PriceCalculator:
+    def __init__(self, strategy: PricingStrategy):
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: PricingStrategy) -> None:
+        self._strategy = strategy
+
+    def calculate_price(self, base_price: float) -> float:
+        if base_price < 0:
+            raise ValueError("Изначальная цена не может быть отрицательной")
+        return self._strategy.calculate(base_price)
+
+
+if __name__ == "__main__":
+    start_price = 100.0
+
+    calculator = PriceCalculator(NoDiscountStrategy())
+    price_no_discount = calculator.calculate_price(start_price)
+    print(f"Изначальная цена: {start_price}, без скидки: {price_no_discount}")
+
+    calculator.set_strategy(PercentageDiscountStrategy(0.70))
+    price_70_percent = calculator.calculate_price(start_price)
+    print(f"Изначальная цена: {start_price}, со скидкой 70%: {price_70_percent}")
+
+    calculator.set_strategy(FixedDiscountStrategy(22.3))
+    price_fix_discount = calculator.calculate_price(start_price)
+    print(f"Изначальная цена: {start_price}, со скидкой 22.3 рубля: {price_fix_discount}")
+
+
+
