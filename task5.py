78650203908c146692512278e80c4dd0969ec6a3
sokolenok <skls77@mail.ru>
2025-11-20 19:20:19 +0300

    Create successful tests

--- task5.py
+++ task5.py
@@ -1,8 +1,16 @@
 import unittest
 from task2 import Counter
 
+
 class TestCounter(unittest.TestCase):
-    pass
+    def test_initial_value(self):
+        counter = Counter(5)
+        self.assertEqual(counter.get_value(), 5)
+
+    def test_increment_plus_step(self):
+        counter = Counter(10)
+        counter.increment(3)
+        self.assertEqual(counter.get_value(), 13)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
